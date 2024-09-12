e87225b73ddc05215c75de0a3213be1f9cfe08ab
CamilaGon <cg561351@gmail.com>
2024-09-11 21:10:17 -0300

    Inicia, req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,28 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ingrediente1 = Ingredient("farinha")
+    assert ingrediente1.name == "farinha"
+    assert ingrediente1.restrictions == {Restriction.GLUTEN}
+
+    ingrediente2 = Ingredient("presunto")
+    assert ingrediente2.name == "presunto"
+    assert ingrediente2.restrictions == {Restriction.ANIMAL_MEAT,
+                                         Restriction.ANIMAL_DERIVED}
+
+    assert repr(ingrediente1) == "Ingredient('farinha')"
+    assert repr(ingrediente2) == "Ingredient('presunto')"
+
+    assert ingrediente1 == Ingredient("farinha")
+    assert ingrediente1 != ingrediente2
+
+    assert hash(ingrediente1) == hash(Ingredient("farinha"))
+    assert hash(ingrediente1) != hash(ingrediente2)
+
+    ingrediente3 = Ingredient("água")
+    assert ingrediente3.name == "água"
+    assert ingrediente3.restrictions == set()
+
+    assert ingrediente3 != ingrediente1
+    assert ingrediente3 != ingrediente2
